1cd8b3acb4d98f534b391157e27b453bb76e4504
nhuanthi <tlam1288@gmail.com>
2020-07-11 14:51:07 -0700

    added function to writefileasync in /api/notes. added return response for api.notes

--- server.js
+++ server.js
@@ -12,12 +12,48 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+const notes = [
+  {
+    id: 1,
+    test: "hello!",
+  },
+];
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  //const newNote = req.body;
+  return res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  notes.push(newNote);
+
+  writeFileAsync(
+    path.join(__dirname, "/db/db.json"),
+    JSON.stringify(notes)
+  ).then(function (error) {
+    if (error) {
+      console.log(error);
+    }
+  });
+
+  return res.json(true);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const chosen = req.params.character;
+
+  console.log(chosen);
+
+  return res.json(false);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(PORT, () => {
