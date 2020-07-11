ef2ae43965d7a50bc1fd41f61ba5b16357ada7e1
nhuanthi <tlam1288@gmail.com>
2020-07-11 13:44:54 -0700

    imported util

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
