1ce94e435b6f9b4341b47292734fa801bc464031
nhuanthi <tlam1288@gmail.com>
2020-07-13 15:11:09 -0700

    added / path to link to index.html

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const notes = [];
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
